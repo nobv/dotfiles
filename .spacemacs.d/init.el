2a530197e4f9ca450612dc5b3a1e5a30650db28a
nobv <6e6f6275@gmail.com>
2019-05-28 08:03:50 +0900

    Update

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -73,9 +73,7 @@ values."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '(
-                                      (lsp-haskell :location (recipe :fetcher github :repo "emacs-lsp/lsp-haskell"))
-                                      )
+   dotspacemacs-additional-packages '()
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
    ;; A list of packages that will not be installed and loaded.
