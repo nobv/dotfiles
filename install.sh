4b245b29542a7d5a3b7190c8e65a5c5616c0e59f
nobv <6e6f6275@gmail.com>
2019-11-12 23:47:45 +0900

    Updated install

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ do
     [[ "$f" == ".git" ]] && continue
     [[ "$f" == ".DS_Store" ]] && continue
     [[ "$f" == ".bashrc" ]] && continue
+    [[ "$f" == ".emacs.d" ]] && continue
 
     #echo "$f"
     ln -snfv ~/.dotfiles/"$f" ~/
