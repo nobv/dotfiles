02b596c56a41797e2dee036a03e7e3977a4292dc
nobv <6e6f6275@gmail.com>
2020-03-10 23:49:44 +0900

    Update emacs configs

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,4 +1,4 @@
-(("default" . ((user-emacs-directory . "~/.emacs.d")))
+(("default" . ((user-emacs-directory . "~/.emacs.d/emacs")))
  ("spacemacs" . ((user-emacs-directory . "~/.emacs.d/spacemacs")
                          (env . (("SPACEMACSDIR" . "~/.spacemacs.d")))))
  ("doom" . ((user-emacs-directory . "~/.emacs.d/doom-emacs"))))
