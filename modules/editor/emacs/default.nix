1102f085846544a81518d5a0da68d0af11d7ef1f
nobv <36393714+nobv@users.noreply.github.com>
2022-05-19 11:08:44 +0900

    Updated emacs

--- modules/editor/emacs/default.nix
+++ modules/editor/emacs/default.nix
@@ -1,33 +1,40 @@
 { pkgs, ... }:
 {
-  home.file = {
-    ".emacs-profiles.el" = {
-      text = builtins.readFile ~/.dotfiles/modules/editor/emacs/.emacs-profiles.el;
-    };
+  home = {
+    packages = with pkgs; [
+      emacs28Packages.lsp-pyright
+    ];
 
-    # https://github.com/plexus/chemacs2
-    # Emacs profile switcher
-    "emacs" = {
-      recursive = true;
-      target = ".emacs.d";
-      source = pkgs.fetchFromGitHub {
-        owner = "plexus";
-        repo = "chemacs2";
-        rev = "ef82118824fac2b2363d3171d26acbabe1738326";
-        sha256 = "1gg4aa6dxc4k9d78j8mrrhy0mvhqmly7jxby69518xs9njxh00dq";
+    file = {
+      ".emacs-profiles.el" = {
+        text = builtins.readFile ~/.dotfiles/modules/editor/emacs/.emacs-profiles.el;
       };
+
+      # https://github.com/plexus/chemacs2
+      # Emacs profile switcher
+      "emacs" = {
+        recursive = true;
+        target = ".emacs.d";
+        source = pkgs.fetchFromGitHub {
+          owner = "plexus";
+          repo = "chemacs2";
+          rev = "ef82118824fac2b2363d3171d26acbabe1738326";
+          sha256 = "1gg4aa6dxc4k9d78j8mrrhy0mvhqmly7jxby69518xs9njxh00dq";
+        };
+      };
+
+      #   "doom" = {
+      #     recursive = true;
+      #     target = ".config/doom";
+      #     source = ~/.dotfiles/configs/emacs/.doom.d;
+      #   };
+
+      #   "spacemacs" = {
+      #     recursive = true;
+      #     target = ".config/spacemacs";
+      #     source = ~/.dotfiles/configs/emacs/.spacemacs.d;
+      #   };
     };
-
-    #   "doom" = {
-    #     recursive = true;
-    #     target = ".config/doom";
-    #     source = ~/.dotfiles/configs/emacs/.doom.d;
-    #   };
-
-    #   "spacemacs" = {
-    #     recursive = true;
-    #     target = ".config/spacemacs";
-    #     source = ~/.dotfiles/configs/emacs/.spacemacs.d;
-    #   };
   };
+
 }
