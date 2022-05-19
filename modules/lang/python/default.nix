e5357b0f2b59e9ae0abe970a918cbb8dd083d19e
nobv <36393714+nobv@users.noreply.github.com>
2022-05-19 21:34:04 +0900

    Updated python

--- modules/lang/python/default.nix
+++ modules/lang/python/default.nix
@@ -13,8 +13,8 @@ in
 
     # error: Package ‘python3.10-pyopenssl-22.0.0’ in /nix/store/iqnx2dai0wzhi5r3savgivg7994icag2-nixpkgs-unstable/nixpkgs-unstable/pkgs/development/python-modules/pyopenssl/default.nix:73 is marked as broken, refusing to evaluate.
     # エラーが解消されたら unstable にしたい
-    unstable.python310Packages.black
-    # stable.black
+    # unstable.python310Packages.black
+    stable.black
 
     #   (ps:
     #     with ps; [
