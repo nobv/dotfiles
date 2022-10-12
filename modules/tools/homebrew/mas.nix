03b84d49fe6121a2d37536e3cb3f32c572b87890
nobv <36393714+nobv@users.noreply.github.com>
2022-10-12 20:57:00 +0900

    Updated mas

--- modules/tools/homebrew/mas.nix
+++ modules/tools/homebrew/mas.nix
@@ -1,12 +1,13 @@
 { pkgs, ... }: {
   homebrew.masApps = {
-    "LINE" = 539883307;
     "Amphetamine" = 937984704;
+    "Day One" = 1055511498;
     "Disk Diag" = 672206759;
     "Endel" = 1484348796;
     "Fuwari" = 1187652334;
     "Highlights" = 1498912833;
     "Kindle" = 405399194;
+    "LINE" = 539883307;
     "LiquidText" = 922765270;
     "MarginNote 3" = 1423522373;
     "Messenger" = 1480068668;
@@ -18,11 +19,5 @@
     "TextSniper" = 1528890965;
     "The Unarchiver" = 425424353;
     "iThoughtsX" = 720669838;
-    # "1Password 7" = 1333542190;
-    # "LastPass" = 926036361;
-    # "Magnet" = 441258766;
-    # "Reeder" = 1529448980;
-    # "Taskade" = 1490048917;
-    # "Todoist" = 585829637;
   };
 }
