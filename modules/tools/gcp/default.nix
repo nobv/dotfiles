b1d368a57170e668b98600fa8489183729e8fefd
nobv <36393714+nobv@users.noreply.github.com>
2022-11-02 22:25:08 +0900

    Updated gcp

--- modules/tools/gcp/default.nix
+++ modules/tools/gcp/default.nix
@@ -1,7 +1,9 @@
 { config, pkgs, lib, ... }:
 {
   home.packages = with pkgs; [
-    google-cloud-sdk
-    cloud-sql-proxy
+    (google-cloud-sdk.withExtraComponents [
+      google-cloud-sdk.components.gke-gcloud-auth-plugin
+      google-cloud-sdk.components.cloud_sql_proxy
+    ])
   ];
 }
