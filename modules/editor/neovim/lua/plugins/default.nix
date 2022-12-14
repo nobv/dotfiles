5813ee0df609ac009953362a4dfed071c6473eb7
nobv <36393714+nobv@users.noreply.github.com>
2022-12-15 00:05:10 +0900

    Moved plugins

--- modules/editor/neovim/lua/plugins/default.nix
+++ modules/editor/neovim/lua/plugins/default.nix
@@ -10,12 +10,12 @@
     vim-nix # https://github.com/LnL7/vim-nix
     purescript-vim # https://github.com/purescript-contrib/purescript-vim
 
-    ## coc
+    ## LSP
     coc-pyright # https://github.com/fannheyward/coc-pyright
-    coc-lists # https://github.com/neoclide/coc-lists
 
     # UI
     dracula-vim # https://github.com/dracula/vim/
     dracula-nvim # https://github.com/Mofiqul/dracula.nvim/
+    lualine-nvim # https://github.com/nvim-lualine/lualine.nvim
   ];
 }
