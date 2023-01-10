9366963b7564404da3d70a42d57145fb2ab30f41
nobv <36393714+nobv@users.noreply.github.com>
2023-01-11 00:47:27 +0900

    Updated zsh

--- modules/term/zsh/aliases.nix
+++ modules/term/zsh/aliases.nix
@@ -1,75 +1,96 @@
 {
-  # Application
-  chrome = "open -a Google Chrome";
 
-  # bat
-  ## use bat instead of cat
+  # Applications {{{
+  chrome = "open -a Google Chrome";
+  #  }}}
+
+  # Alternatives {{{
+  ## bat (instead of cat)
   cat = "bat";
 
-  # Change directory
+  ## procs (instead of ps)
+  ps = "procs";
+
+  ## exa (instead of ls)
+  l = "exa --git --icons -a";
+  ll = "exa --long --header --git --icons -a";
+  ls = "exa";
+
+  ## }}}
+
+  # Shortcusts {{{
+
+  logseq = "cd ~/src/src/github.com/nobv/logs";
+  dots = "cd ~/.dotfiles";
+
+  ## cp
+  cp = "cp -i";
+
+  ## exec
+  reload = "exec $SHELL -l";
+
+  ## mv
+  mv = "mv -i";
+
+  ## rm
+  rm = "rm -i";
+  rmrf = "rm -rf";
+
+  ##  docker {{{
+  d = "docker";
+  dprune = "docker system prune --all --volumes --force && docker builder prune --all --force";
+  ## }}}
+
+  ##  kubernetes {{{
+  k = "kubectl";
+  kx = "kubectx";
+  kn = "kubens";
+  ## }}}
+
+
+  ##  docker compose {{{
+  dc = "docker compose";
+  dcps = "dc ps";
+  dcdown = "dc down";
+  ## }}}
+
+  # terraform {{{
+  tfa = "terraform apply";
+  tfi = "teffaform init";
+  tfp = "terraform plan";
+  ## }}}
+
+  # terragrunt {{{
+  tg = "terragrunt";
+  ## }}}
+  ## }}}
+
+  # Change directory {{{
   ".." = "cd ..";
   "..." = "cd ../..";
   "...." = "cd ../../..";
   "....." = "cd ../../../..";
   "......" = "cd ../../../../..";
+  # }}}
 
-  # logseq = "cd ~/Library/Mobile\\ Documents//iCloud~com~logseq~logseq/Documents/logseq";
-  logseq = "cd ~/Google\\ Drive/My\\ Drive/me/notes/logseq/log";
-  dots = "cd ~/.dotfiles";
-
-  # cp
-  cp = "cp -i";
-
-  # docker
-  d = "docker";
-
-  dprune = "docker system prune --all --volumes --force && docker builder prune --all --force";
-
-  # docker compose
-  dc = "docker compose";
-  dcps = "dc ps";
-  dcdown = "dc down";
-
-  # exa
-  ## use exa instead of ls
-  l = "exa --git --icons -a";
-  ll = "exa --long --header --git --icons -a";
-  ls = "exa";
-
-  # exec
-  reload = "exec $SHELL -l";
-
-  # kubernetes
-  k = "kubectl";
-  kx = "kubectx";
-  kn = "kubens";
-
-  # nix
-  nix-repl = "nix repl '<nixpkgs>'";
-
-  # mv
-  mv = "mv -i";
-
-  # peco
+  # peco {{{
   g = "REPO=$(ghq list | sort -u | peco);for GHQ_ROOT in $(ghq root -all);do [ -d $GHQ_ROOT/$REPO ] && cd $GHQ_ROOT/$REPO;done";
   app = ''
     open "$(ls ~/Applications | peco | awk '{print $3}')"
   '';
+  # }}}
 
-  # procs
-  ## use procs instead of ps
-  ps = "procs";
+  # Nix {{{
 
-  # rm
-  rm = "rm -i";
-  rmrf = "rm -rf";
+  ## home-manager
+  reloadvim = "home-manager switch && vim";
 
-  # terraform
-  tfa = "terraform apply";
-  tfi = "teffaform init";
-  tfp = "terraform plan";
+  ##  nix {{{
+  nix-repl = "nix repl '<nixpkgs>'";
+  ## }}}
 
-  # terragrunt
-  tg = "terragrunt";
+  # }}}
+
 }
 
+# vim: set foldmethod=marker :
