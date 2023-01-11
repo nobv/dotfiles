e977fcc951e2b7c3c448e9e4315c06e09dace7e1
nobv <36393714+nobv@users.noreply.github.com>
2023-01-12 00:14:16 +0900

    Updated gitsigns

--- modules/editor/neovim/after/plugin/gitsigns.lua
+++ modules/editor/neovim/after/plugin/gitsigns.lua
@@ -8,3 +8,7 @@ end
 gitsigns.setup {
     current_line_blame = true,
 }
+
+-- Change color
+-- https://github.com/lewis6991/gitsigns.nvim/wiki/FAQ#how-do-i-change-the-color-of-x
+vim.api.nvim_set_hl(0, 'GitsignsCurrentLineBlame', { fg = 'DimGray',})
