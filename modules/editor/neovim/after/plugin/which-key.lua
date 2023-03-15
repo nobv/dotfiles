8f79d50da2c8921b3f4e7db089d74519da9731a7
nobv <36393714+nobv@users.noreply.github.com>
2023-03-16 02:32:29 +0900

    Updated keymaps

--- modules/editor/neovim/after/plugin/which-key.lua
+++ modules/editor/neovim/after/plugin/which-key.lua
@@ -4,5 +4,72 @@ if (not status) then
   return
 end
 
+which_key.register({
+  ["<leader>"] = {
+    ["<TAB>"] = { ":b#<CR>", "Last buffer" },
+    -- ["<TAB>"] = {
+    --   name = "+workspace",
+    --   n = {":tabn<CR>", "Next tab"},
+    --   o = {":tabnew<CR>", "Oepn new tab"},
+    --   p = {":tabp<CR>", "Previous tab"},
+    --   x = {":tabclose<CR>", "Close current tab"},
+    -- },
+
+    b = {
+      name = "+buffer",
+      b = { ":Telescope buffers<CR>", "Search buffer" },
+      d = { ":bd<CR>", "Delete buffer" },
+    },
+
+    f = {
+      name = "+file",
+      f = { ":Telescope file_browser path=%:p:h select_buffer=true<CR>", "Find file" },
+      t = {
+        name = "NvimTree",
+        t = { ":NvimTreeToggle<CR>", "toggle" },
+        f = { ":NvimTreeFindFileToggle<CR>", "find file" }
+      }
+    },
+
+    g = {
+      name = "+git",
+      g = { ":Neogit<CR>", "Neogit status" },
+    },
+
+    h = {
+      name = "+help",
+      t = { ":Telescope help_tags<CR>", "help tags" },
+
+    },
+
+    p = {
+      name = "+project",
+      f = { ":Telescope find_files<CR>", "Find file in project" },
+    },
+
+    s = {
+      name = "+search",
+      p = { ":Telescope live_grep<CR>", "Search project" },
+      s = { ":Telescope current_buffer_fuzzy_find<CR>", "Swipe" }
+    },
+
+    t = {
+      name = "+toggle",
+      t = { ":Twilight<CR>", "Twilight" },
+    },
+
+    w = {
+      name = "+window",
+      d = { ":close<CR>", "Close current window" },
+      h = { "<C-w><C-h>", "Move left" },
+      j = { "<C-w><C-j>", "Move bottom" },
+      k = { "<C-w><C-k>", "Move top" },
+      l = { "<C-w><C-l>", "Move right" },
+      s = { "<C-w>s", "Split window below" },
+      v = { "<C-w>v", "Split window right" },
+    },
+
+  }
+})
 
 which_key.setup {}
--- modules/editor/neovim/lua/core/keymaps.lua
+++ modules/editor/neovim/lua/core/keymaps.lua
@@ -8,25 +8,10 @@ local keyset = require("../util").keyset
 keyset("i", "jk", "<ESC>") -- exit insert mode with jk
 
 
--- normal mode
-keyset("n", "<leader>nh", ":noh<CR>") -- clear search highlight
-keyset("n", "x", '"_x"') -- don't copy by x
-keyset("n", "<leader>+", "<C-a>") -- increment
-keyset("n", "<leader>-", "<C-x>") -- decrement
-
---window
-keyset("n", "<leader>wv", "<C-w>v") -- split window vertically
-keyset("n", "<leader>ws", "<C-w>s") -- split window horizontally
-keyset("n", "<leader>wx", ":close<CR>") -- close current split window
-keyset("n", "<leader>wh", "<C-w><C-h>") -- move to left window
-keyset("n", "<leader>wj", "<C-w><C-j>") -- move to bottom window
-keyset("n", "<leader>wk", "<C-w><C-k>") -- move to top window
-keyset("n", "<leader>wl", "<C-w><C-l>") -- move to right window
-
--- tab
-keyset("n", "<leader>to", ":tabnew<CR>") -- open new tab
-keyset("n", "<leader>tx", ":tabclose<CR>") -- close current tab
-keyset("n", "<leader>tn", ":tabn<CR>") -- go to next tab
-keyset("n", "<leader>tp", ":tabp<CR>") -- go to previous tab
-
-
+-- -- normal mode
+-- keyset("n", "<leader>nh", ":noh<CR>") -- clear search highlight
+-- keyset("n", "x", '"_x"') -- don't copy by x
+-- keyset("n", "<leader>+", "<C-a>") -- increment
+-- keyset("n", "<leader>-", "<C-x>") -- decrement
+--
+--
