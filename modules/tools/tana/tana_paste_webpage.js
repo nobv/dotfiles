daf4fe2958de5fb5709446b8ebf186466721d685
nobv <36393714+nobv@users.noreply.github.com>
2023-03-17 11:33:35 +0900

    Updated tana_paste_webpage

--- modules/tools/tana/tana_paste_webpage.js
+++ modules/tools/tana/tana_paste_webpage.js
@@ -3,28 +3,50 @@ javascript: (async function () {
   const formattedHours = String(now.getHours()).padStart(2, "0");
   const formattedMinutes = String(now.getMinutes()).padStart(2, "0");
 
-  let tag;
-  let extra = "";
-  const host = location.hostname;
-  switch (host) {
-    case "www.youtube.com":
-      tag = `#youtube`;
-      extra = `    - Created by:: [[${document.querySelector("#text > a").innerHTML}]]`;
-      break;
-    default:
-      tag = `#[[web page]]`;
-  }
+  const time = `${formattedHours}:${formattedMinutes}`;
 
-  const text = `%%tana%%\n
-- ${formattedHours}:${formattedMinutes}\n
-  - [ ] ${document.title} ${tag}\n
+  let tana_paste = "%%tana%%\n";
+  switch (location.hostname) {
+
+    case "www.youtube.com":
+      tana_paste += `
+- ${time}\n
+  - [ ] ${document.title} #youtube\n
     - Title:: ${document.title}\n
     - URL:: ${location.href}\n
-${extra}`;
+    - Created by:: [[${document.querySelector("#text > a").innerHTML}]]`;
+      break;
+
+    case "github.com":
+      tana_paste += `
+- ${time}\n
+  - [ ] ${document.querySelector("#readme > div.Box-body.px-5.pb-5 > article > h1").innerText} #github\n
+    - Title:: ${document.title}\n
+    - URL:: ${location.href}\n
+    - Repository Owner:: [[${document.querySelector("#repository-container-header > div.d-flex.flex-wrap.flex-justify-end.mb-3.px-3.px-md-4.px-lg-5 > div > div > span.author.flex-self-stretch > a").innerText}]]`;
+      break;
+
+    case "ja.wikipedia.org":
+    case "en.wikipedia.org":
+      tana_paste += `
+- ${time}\n
+  - [ ] ${document.querySelector("#firstHeading > span").innerText} #wiki\n
+    - Title:: ${document.title}\n
+    - URL:: ${location.href}\n`;
+      break;
+
+    default:
+      tana_paste += `
+- ${time}\n
+  - [ ] ${document.title} #[[web page]]\n
+    - Title:: ${document.title}\n
+    - URL:: ${location.href}\n`;
+      break;
+  }
 
   try {
     await navigator.permissions.query({name: "clipboard-write"});
-    await navigator.clipboard.writeText(text);
+    await navigator.clipboard.writeText(tana_paste);
     console.log("copy to clipboard success.");
   } catch (err) {
     console.error("copy to clipboard error:", err);
