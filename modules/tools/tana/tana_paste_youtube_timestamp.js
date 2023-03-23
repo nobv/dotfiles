9dbe888f933bdfae870677d750f0437d398eae79
nobv <36393714+nobv@users.noreply.github.com>
2023-03-24 01:05:15 +0900

    Updated tana paste

--- modules/tools/tana/tana_paste_youtube_timestamp.js
+++ modules/tools/tana/tana_paste_youtube_timestamp.js
@@ -2,15 +2,16 @@ javascript: (async function () {
   const player = document.getElementById("ytd-player");
   player.pause();
 
-  const video = document.getElementsByTagName("video");
-  const current_time = parseInt(video[0].currentTime);
+  const video = document.getElementsByTagName("video")[0];
+  const current_time = parseInt(video.currentTime);
   const current_time_str = document.getElementsByClassName("ytp-time-current")[0].innerText;
 
   const short_link_url = document.querySelectorAll('[rel="shortlinkUrl"]')[0].href;
 
   const tana_paste = `
 %%tana%%\n
-- [ ] [${current_time_str}](${short_link_url}?t=${current_time})\n
+- [${current_time_str}](${short_link_url}?t=${current_time})\n
+  - Screenshot:: \n
   - Memo:: \n`;
 
   try {
