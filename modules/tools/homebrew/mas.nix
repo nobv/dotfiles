6f8388ebe83a7ec79c66081ffb639ede744e8621
nobv <36393714+nobv@users.noreply.github.com>
2023-03-28 00:53:09 +0900

    Updated mas

--- modules/tools/homebrew/mas.nix
+++ modules/tools/homebrew/mas.nix
@@ -4,19 +4,20 @@
     "Day One" = 1055511498;
     "Disk Diag" = 672206759;
     "Endel" = 1484348796;
-    # "Highlights" = 1498912833;
     "Kindle" = 405399194;
     "LINE" = 539883307;
-    # "LiquidText" = 922765270;
     "Messenger" = 1480068668;
-    # "Noted." = 1446580517;
+    "Post-it®" = 1475777828;
     "Slack" = 803453959;
-    # "Spark" = 1176895641;
     "Telegram" = 747648890;
     "TextSniper" = 1528890965;
     "The Unarchiver" = 425424353;
+    "Toggl Track" = 1291898086;
     "iThoughtsX" = 720669838;
     "辞書 by 物書堂" = 1380563956;
-    "Toggl Track" = 1291898086;
+    # "Highlights" = 1498912833;
+    # "LiquidText" = 922765270;
+    # "Noted." = 1446580517;
+    # "Spark" = 1176895641;
   };
 }
