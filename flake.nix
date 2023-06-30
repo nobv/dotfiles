e96b0348419dc9dd59e35a9b3fd56b3253d8aa8b
nobv <36393714+nobv@users.noreply.github.com>
2023-07-01 02:32:21 +0900

    Added flake

---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  description = "nobv's Dotfiles";
+
+  inputs = {
+    # Package sets
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixpkgs-22.05-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    # Environment/system management
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    darwin = {
+      url = "github:lnl7/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Othres
+  };
+
+  outputs = inputs@{ self, home-manager, darwin, ... }:
+    let
+      defaultUserName = "nobv";
+
+      overlays =
+        let path = ./overlays; in
+        with builtins;
+        map (n: import (path + ("/" + n)))
+          (filter
+            (n: match ".*\\.nix" n != null ||
+              pathExists (path + ("/" + n + "/default.nix")))
+            (attrNames (readDir path)));
+
+
+      mkDarwinConfig =
+        { system
+        , userName ? defaultUserName
+        , nixpkgs-stable ? inputs.nixpkgs-stable
+        , nixpkgs ? inputs.nixpkgs
+        , extraModules ? [ ]
+        }:
+        darwin.lib.darwinSystem {
+          system = system;
+          modules = [
+            home-manager.darwinModules.home-manager
+            # {
+            #   home-manager.useGlobalPkgs = true;
+            #   home-manager.useUserPackages = true;
+            #   # home-manager.users.nobv = import ./home-manager/home.nix;
+            # }
+          ] ++ extraModules;
+          # inputs = { inherit darwin nixpkgs inputs overlays; };
+          specialArgs = {
+            inherit self inputs nixpkgs darwin nixpkgs-stable overlays userName;
+          };
+        };
+
+    in
+    {
+      darwinConfigurations = {
+        macmini = mkDarwinConfig {
+          system = "aarch64-darwin";
+          extraModules = [ ./machines/macmini2020 ];
+        };
+      };
+    };
+}
