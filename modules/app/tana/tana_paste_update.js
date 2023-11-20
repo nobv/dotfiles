d5933cda4e91dc1b20bc69719ea3b798fa1d4465
nobv <36393714+nobv@users.noreply.github.com>
2023-11-20 23:29:24 +0900

    Moved to app dir

--- modules/app/tana/tana_paste_update.js
+++ modules/app/tana/tana_paste_update.js
@@ -1,4 +1,4 @@
-javascript: (async function () {
+javascript: (async function() {
   const now = new Date();
   const formattedHours = String(now.getHours()).padStart(2, "0");
   const formattedMinutes = String(now.getMinutes()).padStart(2, "0");
@@ -12,8 +12,20 @@ javascript: (async function () {
     - URL:: ${location.href}\n`;
 
   try {
-    await navigator.permissions.query({name: "clipboard-write"});
+    await navigator.permissions.query({ name: "clipboard-write" });
     await navigator.clipboard.writeText(tana_paste);
+    console.log("copy to clipboard success.");
+  } catch (err) {
+    console.error("copy to clipboard error:", err);
+  }
+})();
+
+javascript: (async function() {
+  const md = `[${document.title}](${location.href})`;
+
+  try {
+    await navigator.permissions.query({ name: "clipboard-write" });
+    await navigator.clipboard.writeText(md);
     console.log("copy to clipboard success.");
   } catch (err) {
     console.error("copy to clipboard error:", err);
--- modules/app/tana/tana_paste_youtube_timestamp.js
+++ modules/app/tana/tana_paste_youtube_timestamp.js
@@ -1,4 +1,4 @@
-javascript: (async function () {
+javascript: (async function() {
   const player = document.getElementById("ytd-player");
   player.pause();
 
@@ -8,18 +8,15 @@ javascript: (async function () {
 
   const videoId = document.querySelector("ytd-watch-flexy").getAttribute("video-id");
 
-  const tana_paste = `
-%%tana%%\n
-- [${current_time_str}](https://youtu.be/${videoId}?t=${current_time})\n
-  - Screenshot:: \n
-  - Memo:: \n`;
+  const timestamp_md = `- [${current_time_str}](https://youtu.be/${videoId}?t=${current_time})\n`;
 
   try {
-    await navigator.permissions.query({name: "clipboard-write"});
-    await navigator.clipboard.writeText(tana_paste);
+    await navigator.permissions.query({ name: "clipboard-write" });
+    await navigator.clipboard.writeText(timestamp_md);
     console.log("copy to clipboard success.");
   } catch (err) {
     console.error("copy to clipboard error:", err);
   }
 })();
+
 
