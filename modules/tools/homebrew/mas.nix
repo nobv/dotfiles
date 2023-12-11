1fd32733a8013a71804b5d5dd1146b0a588de2b6
nobv <36393714+nobv@users.noreply.github.com>
2023-12-11 22:08:47 +0900

    Updated homebrew

--- modules/tools/homebrew/mas.nix
+++ modules/tools/homebrew/mas.nix
@@ -1,12 +1,12 @@
 { pkgs, ... }: {
   homebrew.masApps = {
     "Amphetamine" = 937984704;
-    "Be Focused Pro - Focus Timer" = 961632517;
     "DaisyDisk" = 411643860;
     "Day One" = 1055511498;
     "Evernote" = 406056744;
     "Kindle" = 405399194;
     "LINE" = 539883307;
+    "Magnet" = 441258766;
     "Messenger" = 1480068668;
     "Post-it®" = 1475777828;
     "Slack" = 803453959;
@@ -14,15 +14,17 @@
     "TextSniper" = 1528890965;
     "The Unarchiver" = 425424353;
     "Things 3" = 904280696;
-    "Toggl Track" = 1291898086;
+    "WireGuard" = 1451685025;
     "Xcode" = 497799835;
     "iThoughtsX" = 720669838;
     "辞書 by 物書堂" = 1380563956;
+    # "Be Focused Pro - Focus Timer" = 961632517;
     # "Disk Diag" = 672206759;
     # "Endel" = 1484348796;
     # "Highlights" = 1498912833;
     # "LiquidText" = 922765270;
     # "Noted." = 1446580517;
     # "Spark" = 1176895641;
+    # "Toggl Track" = 1291898086;
   };
 }
