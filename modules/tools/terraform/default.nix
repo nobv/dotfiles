92564ac99af6113438c7d8c11e886080950fd087
nobv <36393714+nobv@users.noreply.github.com>
2024-01-22 23:39:43 +0900

    Updated terraform

--- modules/tools/terraform/default.nix
+++ modules/tools/terraform/default.nix
@@ -1,8 +1,20 @@
-{ config, pkgs, lib, ... }:
+{ lib, system, ... }:
+let
+  pkgs = import <nixpkgs> {
+    config = {
+      allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+        "terraform"
+      ];
+    };
+  };
+in
 {
-  home.packages = with pkgs; [
-    terraform
-    terragrunt
-    terraform-ls
-  ];
+  home.packages = with pkgs;
+    [
+      terraform
+      terragrunt
+      terraform-ls
+    ];
+
+
 }
