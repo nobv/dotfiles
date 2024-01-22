4792e0713c22cdcfed774cb94120e42d1a75524b
nobv <36393714+nobv@users.noreply.github.com>
2024-01-22 23:39:55 +0900

    Updated lua

--- modules/lang/lua/default.nix
+++ modules/lang/lua/default.nix
@@ -3,6 +3,6 @@
   home.packages = with pkgs; [
     lua
     #luajitPackages.lua-lsp # https://github.com/Alloyed/lua-lsp
-    sumneko-lua-language-server # https://github.com/LuaLS/lua-language-server
+    # lua-language-server # https://github.com/LuaLS/lua-language-server
   ];
 }
