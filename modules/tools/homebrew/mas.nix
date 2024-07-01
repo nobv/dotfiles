2f754270a99379cb44568776db845a0d7cde6f86
nobv <36393714+nobv@users.noreply.github.com>
2024-07-01 11:35:01 +0900

    Updated homebrew

--- modules/tools/homebrew/mas.nix
+++ modules/tools/homebrew/mas.nix
@@ -3,22 +3,21 @@
     "Amphetamine" = 937984704;
     "DaisyDisk" = 411643860;
     "Day One" = 1055511498;
-    "Evernote" = 406056744;
     "Fantastical" = 975937182;
-    "Goodnotes 6" = 1444383602;
     "Kindle" = 405399194;
     "LINE" = 539883307;
     "Magnet" = 441258766;
     "Messenger" = 1480068668;
     "Post-it®" = 1475777828;
     "Slack" = 803453959;
+    "Streaks" = 963034692;
     "Telegram" = 747648890;
     "TextSniper" = 1528890965;
     "The Unarchiver" = 425424353;
-    "Things 3" = 904280696;
+    "Toggl Track" = 1291898086;
     "WireGuard" = 1451685025;
     "Xcode" = 497799835;
-    "iThoughtsX" = 720669838;
+    "one sec" = 1532875441;
     "辞書 by 物書堂" = 1380563956;
   };
 }
