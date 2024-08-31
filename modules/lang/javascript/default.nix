bcb25cd01d4de1a3f79391109a21edb998537153
nobv <36393714+nobv@users.noreply.github.com>
2024-08-31 11:52:16 +0900

    feat(javascript): add some packages

--- modules/lang/javascript/default.nix
+++ modules/lang/javascript/default.nix
@@ -1,14 +1,52 @@
 { config, pkgs, lib, ... }:
 {
   # https://github.com/NixOS/nixpkgs/blob/a0dbe47318/doc/languages-frameworks/javascript.section.md
-  home.packages = with pkgs; [
-    nodejs
-    nodePackages.prettier
+  home =
+    {
+      packages = with pkgs; [
+        nodejs
+        nodePackages.prettier
 
-    # Package managers
-    yarn
-    volta
+        # Package managers
+        yarn
+        volta
+        corepack
 
-    nest-cli
-  ];
+        nest-cli
+      ];
+
+
+      sessionVariables = {
+        NPM_CONFIG_PREFIX = "~/.npm-packages";
+      };
+
+      sessionPath = [
+        "~/.npm-packages/bin"
+      ];
+
+
+      file.".npmrc".text = ''
+        prefix=~/.npm-packages
+      '';
+
+      activation = {
+        setupNpmPackage = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+          HOME_DIR="$HOME"
+          NPM_GLOBAL_DIR="$HOME_DIR/.npm-packages"
+            if [ ! -d "$NPM_GLOBAL_DIR" ]; then
+              $DRY_RUN_CMD mkdir -p "$NPM_GLOBAL_DIR"
+              $DRY_RUN_CMD ${pkgs.nodejs}/bin/npm config set prefix "$NPM_GLOBAL_DIR"
+            fi
+        '';
+        installAdminJSCli = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+          HOME_DIR="$HOME"
+          NPM_GLOBAL_DIR="$HOME_DIR/.npm-packages"
+          if [ ! -f "$NPM_GLOBAL_DIR/bin/adminjs" ]; then
+            run echo "Installing AdminJS CLI..."
+            $DRY_RUN_CMD ${pkgs.nodejs}/bin/npm install -g @adminjs/cli
+          fi
+        '';
+      };
+
+    };
 }
