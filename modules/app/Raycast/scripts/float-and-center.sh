9d6f59828160dcbae9740d51c1c47ad684028983
nobv <36393714+nobv@users.noreply.github.com>
2025-06-13 07:59:49 +0900

    feat: add Raycast float-and-center script

---
+++ modules/app/Raycast/scripts/float-and-center.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Float and center
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author nobv
+# @raycast.authorURL https://raycast.com/nobv
+
+
+aerospace layout floating
+open -g raycast://extensions/raycast/window-management/center
