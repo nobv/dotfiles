f9282be56bbbd372cd36266d680638091df7ee2a
nobv <36393714+nobv@users.noreply.github.com>
2025-06-16 14:51:19 +0900

    fix(emacs): resolve home-manager users duplication error

--- modules/editors/emacs/default.nix
+++ modules/editors/emacs/default.nix
@@ -11,31 +11,33 @@ in
   };
 
   config = mkIf cfg.enable {
-    home-manager.users.${username}.programs.emacs = {
-      enable = true;
-    };
+    home-manager.users.${username} = {
+      programs.emacs = {
+        enable = true;
+      };
 
-    home-manager.users.${username}.home = {
-      packages = with pkgs; [
-        # emacs28Packages.lsp-pyright
-      ];
+      home = {
+        packages = with pkgs; [
+          # emacs28Packages.lsp-pyright
+        ];
 
-      file = {
-        # https://github.com/plexus/chemacs2
-        # Emacs profile switcher
-        "emacs" = {
-          recursive = true;
-          target = ".emacs.d";
-          source = pkgs.fetchFromGitHub {
-            owner = "plexus";
-            repo = "chemacs2";
-            rev = "ef82118824fac2b2363d3171d26acbabe1738326";
-            sha256 = "1gg4aa6dxc4k9d78j8mrrhy0mvhqmly7jxby69518xs9njxh00dq";
+        file = {
+          # https://github.com/plexus/chemacs2
+          # Emacs profile switcher
+          "emacs" = {
+            recursive = true;
+            target = ".emacs.d";
+            source = pkgs.fetchFromGitHub {
+              owner = "plexus";
+              repo = "chemacs2";
+              rev = "ef82118824fac2b2363d3171d26acbabe1738326";
+              sha256 = "1gg4aa6dxc4k9d78j8mrrhy0mvhqmly7jxby69518xs9njxh00dq";
+            };
           };
-        };
 
-        ".emacs-profiles.el" = {
-          text = builtins.readFile ./.emacs-profiles.el;
+          ".emacs-profiles.el" = {
+            text = builtins.readFile ./.emacs-profiles.el;
+          };
         };
       };
     };
