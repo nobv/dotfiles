79cacd784947af20744b9f6942ead990596a057e
nobv <36393714+nobv@users.noreply.github.com>
2025-09-15 03:03:02 +0900

    refactor(overlays): reformat function arguments and improve readability in default.nix files

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,34 +2,44 @@
 self: super: {
 
   installApplication =
-    { name
-    , appname ? name
-    , version
-    , src
-    , description
-    , homepage
-    , postInstall ? ""
-    , sourceRoot ? "."
-    , ...
+    {
+      name,
+      appname ? name,
+      version,
+      src,
+      description,
+      homepage,
+      postInstall ? "",
+      sourceRoot ? ".",
+      ...
     }:
-      with super; stdenv.mkDerivation {
-        name = "${name}-${version}";
-        version = "${version}";
-        src = src;
-        buildInputs = [ undmg unzip ];
-        sourceRoot = sourceRoot;
-        phases = [ "unpackPhase" "installPhase" ];
-        installPhase = ''
+    with super;
+    stdenv.mkDerivation {
+      name = "${name}-${version}";
+      version = "${version}";
+      src = src;
+      buildInputs = [
+        undmg
+        unzip
+      ];
+      sourceRoot = sourceRoot;
+      phases = [
+        "unpackPhase"
+        "installPhase"
+      ];
+      installPhase =
+        ''
           mkdir -p "$out/Applications/${appname}.app"
           cp -pR * "$out/Applications/${appname}.app"
-        '' + postInstall;
-        meta = with super.lib; {
-          description = description;
-          homepage = homepage;
-          maintainers = with maintainers; [ nobv ];
-          platforms = platforms.darwin;
-        };
+        ''
+        + postInstall;
+      meta = with super.lib; {
+        description = description;
+        homepage = homepage;
+        maintainers = with maintainers; [ nobv ];
+        platforms = platforms.darwin;
       };
+    };
 
   # JupyterLab = self.installApplication
   #   rec {
@@ -46,7 +56,6 @@ self: super: {
   #     '';
   #     homepage = "https://github.com/jupyterlab/jupyterlab-desktop";
   #   };
-
 
   # ThingsHelper = self.installApplication
   #   rec{
