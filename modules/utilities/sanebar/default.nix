3e273aaa4af01da5826e0487b161f4fe2160f820
nobv <36393714+nobv@users.noreply.github.com>
2026-03-02 20:39:47 +0900

    feat(utilities): add sanebar module

---
+++ modules/utilities/sanebar/default.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, lib, ... }:
+
+with lib;
+
+let
+  cfg = config.modules.utilities.sanebar;
+in
+{
+  options.modules.utilities.sanebar = {
+    enable = mkEnableOption "sanebar application";
+  };
+
+  config = mkIf cfg.enable {
+    homebrew = mkIf (config.modules.system.homebrew.enable or false) {
+      casks = [ "sanebar" ];
+    };
+  };
+}
