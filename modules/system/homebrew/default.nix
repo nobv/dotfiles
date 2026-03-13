d0add224c345944821ecdbcf1cc37c6cd8ccc43a
nobv <36393714+nobv@users.noreply.github.com>
2026-03-13 21:15:59 +0900

    fix(homebrew): rename brewPrefix to prefix

--- modules/system/homebrew/default.nix
+++ modules/system/homebrew/default.nix
@@ -17,7 +17,7 @@ in
 
   config = mkIf cfg.enable {
     homebrew = {
-      brewPrefix = "/opt/homebrew/bin";
+      prefix = "/opt/homebrew";
       enable = true;
       onActivation = {
         autoUpdate = true;
