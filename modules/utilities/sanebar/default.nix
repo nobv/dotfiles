9619dcc43889ff6264ddfe28d0523a25e58b93aa
nobv <36393714+nobv@users.noreply.github.com>
2026-03-27 10:24:56 +0900

    fix(sanebar): add missing tap for sanebar

--- modules/utilities/sanebar/default.nix
+++ modules/utilities/sanebar/default.nix
@@ -16,6 +16,7 @@ in
 
   config = mkIf cfg.enable {
     homebrew = mkIf (config.modules.system.homebrew.enable or false) {
+      taps = [ "sane-apps/tap" ];
       casks = [ "sane-apps/tap/sanebar" ];
     };
   };
